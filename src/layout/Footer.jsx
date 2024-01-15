22322e8ad6a3620acc813e68ccb0c96ac3681751
jeru7 <john.ungab7@gmail.com>
2024-01-15 14:33:25 +0800

    footer done

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,10 +1,25 @@
 import "./layout.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedinIn, faGithub } from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footer-icons"></div>
-      <div className="footer-name"></div>
+      <div className="footer-icons">
+        <a
+          href="https://www.linkedin.com/in/jeru7/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FontAwesomeIcon className="icon" icon={faLinkedinIn} />
+        </a>
+        <a href="https://github.com/jeru7" target="_blank" rel="noreferrer">
+          <FontAwesomeIcon className="icon" icon={faGithub} />
+        </a>
+      </div>
+      <div className="footer-name">
+        Developed by: <em>jeru7</em>
+      </div>
     </footer>
   );
 };
