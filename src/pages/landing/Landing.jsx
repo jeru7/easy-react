6096db88ee40d8f32a039918cfec3489b8b30f64
jeru7 <john.ungab7@gmail.com>
2024-01-17 16:13:46 +0800

    slider animation done

--- src/pages/landing/Landing.jsx
+++ src/pages/landing/Landing.jsx
@@ -1,13 +1,52 @@
 import "./landing.css";
+import { Link } from "react-router-dom";
 const Landing = () => {
   return (
     <div className="landing-page">
       <h1 className="landing-title">EasyReactProjects</h1>
-      <div className="landing-projects">
-        <div className="project"></div>
-        <div className="project"></div>
-        <div className="project"></div>
-        <div className="project"></div>
+      <div className="slider">
+        <ul className="landing-projects">
+          <Link to="/todo">
+            <li className="project">
+              <p>ToDoList</p>
+            </li>
+          </Link>
+          <Link to="/quote">
+            <li className="project">
+              <p>QuoteGen</p>
+            </li>
+          </Link>
+          <Link to="weather">
+            <li className="project">
+              <p>CheckWeather</p>
+            </li>
+          </Link>
+          <Link to="book">
+            <li className="project">
+              <p>BookShelf</p>
+            </li>
+          </Link>
+          <Link to="/todo">
+            <li className="project">
+              <p>ToDoList</p>
+            </li>
+          </Link>
+          <Link to="/quote">
+            <li className="project">
+              <p>QuoteGen</p>
+            </li>
+          </Link>
+          <Link to="weather">
+            <li className="project">
+              <p>CheckWeather</p>
+            </li>
+          </Link>
+          <Link to="book">
+            <li className="project">
+              <p>BookShelf</p>
+            </li>
+          </Link>
+        </ul>
       </div>
     </div>
   );
