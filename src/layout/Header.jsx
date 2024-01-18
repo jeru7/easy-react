d56a226c0119d2b93c26cbfcf7cbd68521befac7
jeru7 <john.ungab7@gmail.com>
2024-01-18 21:43:57 +0800

    responsive header done

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,31 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./layout.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faMinus } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <nav className="navbar">
+    <nav className={`navbar ${isOpen ? "active" : ""}`}>
       <Link to="/" className="navbar-logo">
         <div className="navbar-logo-text">EasyReact</div>
       </Link>
-      <div className="navbar-list">
+      <div
+        className={`navbar-list ${isOpen ? "active" : ""}`}
+        style={{ display: isOpen ? "flex" : "" }}
+      >
         <ul>
-          <Link to="/" className="home">
+          <Link to="/" className={`home ${isOpen ? "active" : ""}`}>
             <li>Home</li>
           </Link>
-          <Link to="/todo" className="todo">
+          <Link to="/todo" className={`todo ${isOpen ? "active" : ""}`}>
             <li>ToDoList</li>
           </Link>
-          <Link to="/quote" className="quote">
+          <Link to="/quote" className={`quote ${isOpen ? "active" : ""}`}>
             <li>QuoteGen</li>
           </Link>
-          <Link to="/weather" className="weather">
+          <Link to="/weather" className={`weather ${isOpen ? "active" : ""}`}>
             <li>CheckWeather</li>
           </Link>
-          <Link to="/books" className="book">
+          <Link to="/books" className={`book ${isOpen ? "active" : ""}`}>
             <li>BookShelf</li>
           </Link>
         </ul>
+      </div>
+      <div className="navbar-menu" onClick={handleMenu}>
+        <FontAwesomeIcon
+          className="menu-icon"
+          icon={isOpen ? faMinus : faBars}
+        />
       </div>
     </nav>
   );
