ac3fade27c3baf13b52753b089d4c22eb144fd27
jeru7 <john.ungab7@gmail.com>
2024-01-20 23:03:26 +0800

    todo layout dones

--- src/pages/todo/TodoList.jsx
+++ src/pages/todo/TodoList.jsx
@@ -1,5 +1,25 @@
+// import { useState } from "react";
+import todo from "./todo.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+
 const TodoList = () => {
-  return <div className="todolist">todo</div>;
+  // const [isEmpty, setIsEmpty] = useState(false);
+
+  return (
+    <div className={todo.todoWrapper}>
+      <div className={todo.todoContainer}>
+        <h1 className={todo.todoTitle}>ToDoList</h1>
+        <div className={todo.todoInput}>
+          <input type="text" placeholder="Enter your todo here..." />
+          <button>
+            <FontAwesomeIcon icon={faPlus} />
+          </button>
+        </div>
+        <div className={todo.todos}></div>
+      </div>
+    </div>
+  );
 };
 
 export default TodoList;
