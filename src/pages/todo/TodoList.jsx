85290c35935bf65cee76358b50ef727bbcd653a5
jeru7 <john.ungab7@gmail.com>
2024-01-27 16:28:42 +0800

    add todo function done

--- src/pages/todo/TodoList.jsx
+++ src/pages/todo/TodoList.jsx
@@ -1,71 +1,59 @@
-import todo from "./todo.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus, faTrash, faEdit } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
 
 const TodoList = () => {
   const [todoList, setTodoList] = useState([]);
-  const [isEmpty, setIsEmpty] = useState(true);
-  const [todos, setTodos] = useState("");
+  const [todoInput, setTodoInput] = useState("");
 
-  const handleInputChange = (e) => {
-    setTodos(e.target.value);
-  };
-
-  const handleEdit = (index) => {
-    // todo
-  };
-
-  const handleAddTodo = () => {
-    if (todos.trim() !== "") {
-      setTodoList([...todoList, todos]);
-      setTodos("");
-      setIsEmpty(false);
-    } else {
-      setIsEmpty(true);
+  const handleSetTodo = (e) => {
+    e.preventDefault();
+    if (todoInput !== "") {
+      setTodoList([...todoList, todoInput]);
+      setTodoInput("");
     }
   };
 
   const handleDelete = (index) => {
-    const newTodoList = todoList.filter((_, i) => i !== index);
-    setTodoList(newTodoList);
-    if (newTodoList.length === 0) {
-      setIsEmpty(true);
-    }
+    let newList = todoList.filter((_, i) => i !== index);
+    setTodoList(newList);
   };
+
+  const handleEdit = () => {};
+
   return (
-    <div className={todo.todoWrapper}>
-      <div className={todo.todoContainer}>
-        <h1 className={todo.todoTitle}>ToDoList</h1>
-        <div className={todo.todoInput}>
+    <div className="todo">
+      <div className="todo__container">
+        <div className="todo__input">
           <input
             type="text"
-            placeholder="Enter your todo here..."
-            onChange={handleInputChange}
+            id="todoInput"
+            placeholder="Enter your todo..."
+            value={todoInput}
+            onChange={(e) => setTodoInput(e.target.value)}
           />
-          <button onClick={handleAddTodo}>
-            <FontAwesomeIcon icon={faPlus} />
+          <button id="addTaskBtn" onClick={handleSetTodo}>
+            <FontAwesomeIcon icon={faPlus}></FontAwesomeIcon>
           </button>
         </div>
-        <div className={todo.todos}>
-          {isEmpty && (
-            <p className={todo.errorMsg}>Todo list is currently empty.</p>
-          )}
-          <ul>
-            {todoList.map((todoItem, index) => (
-              <li key={index}>
-                {todoItem}
+        <div className="todo__display">
+          {todoList.map((todo, index) => (
+            <div key={index} className="todo__display-item">
+              <p className="todo-name">{todo}</p>
+              <div className="todo-icons">
                 <FontAwesomeIcon
                   icon={faEdit}
-                  onClick={() => handleEdit(index)}
-                />
+                  onClick={handleEdit}
+                  className="icons"
+                ></FontAwesomeIcon>
                 <FontAwesomeIcon
                   icon={faTrash}
                   onClick={() => handleDelete(index)}
-                />
-              </li>
-            ))}
-          </ul>
+                  className="icons"
+                ></FontAwesomeIcon>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
